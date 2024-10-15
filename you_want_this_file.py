9c1bebbb4a19443b077e9d76c60c304c8cbeb925
Jurrien Vos <Jurrien.Vos@ses.com>
2024-10-15 15:56:56 +0200

    extra lines

--- you_want_this_file.py
+++ you_want_this_file.py
@@ -11,6 +11,45 @@
 
 #mod9
 
+
 #gorka1
 #gorka2
 #mod1
+
+age=2
+if age<0:
+    raise ValueError("no possible")
+
+
+age = -1
+try:
+    if age < 0:
+        raise ValueError("no possible")
+except ValueError as e:
+    print(f"An error occurred: {e}")
+
+
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+
+try:
+    file = open("data.txt", "r")
+except FileNotFoundError:
+    print("File not found!")
+finally:
+    print("Execution not completed.")
+
+
+class CustomError(Exception):
+    pass
+try:
+    raise CustomError("This is a custom error!")
+except CustomError as e:
+    print(e)
+
+
+#extra lines
+
+
